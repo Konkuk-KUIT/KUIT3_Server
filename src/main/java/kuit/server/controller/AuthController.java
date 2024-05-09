7771d2e883991c0b457aed4b41c365e9f9ff42c7
kmw10693 <kmw10693@konkuk.ac.kr>
2024-05-09 23:14:05 +0900

    feat: 로그인, JWT 검증 테스트 구현

---
+++ src/main/java/kuit/server/controller/AuthController.java
@@ -0,0 +1,47 @@
+package kuit.server.controller;
+
+import kuit.server.common.argument_resolver.PreAuthorize;
+import kuit.server.common.exception.UserException;
+import kuit.server.common.response.BaseResponse;
+import kuit.server.dto.auth.LoginRequest;
+import kuit.server.dto.auth.LoginResponse;
+import kuit.server.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import static kuit.server.common.response.status.BaseExceptionResponseStatus.INVALID_USER_VALUE;
+import static kuit.server.util.BindingResultUtils.getErrorMessages;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    /**
+     * 로그인
+     */
+    @PostMapping("/login")
+    public BaseResponse<LoginResponse> login(@Validated @RequestBody LoginRequest authRequest, BindingResult bindingResult) {
+        log.info("[AuthController.login]");
+        if (bindingResult.hasErrors()) {
+            throw new UserException(INVALID_USER_VALUE, getErrorMessages(bindingResult));
+        }
+        return new BaseResponse<>(authService.login(authRequest));
+    }
+
+    /**
+     * 인가(JWT 검증) 테스트
+     */
+    @GetMapping("/test")
+    public BaseResponse<String> checkAuthorization(@PreAuthorize long userId) {
+        log.info("[AuthController.checkAuthorization]");
+        return new BaseResponse<>("userId=" + userId);
+    }
+
+}
---
+++ src/main/java/kuit/server/service/AuthService.java
@@ -0,0 +1,63 @@
+package kuit.server.service;
+
+import kuit.server.common.exception.UserException;
+import kuit.server.common.exception.jwt.unauthorized.JwtUnauthorizedTokenException;
+import kuit.server.dao.UserDao;
+import kuit.server.dto.auth.LoginRequest;
+import kuit.server.dto.auth.LoginResponse;
+import kuit.server.util.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import static kuit.server.common.response.status.BaseExceptionResponseStatus.*;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserDao userDao;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public LoginResponse login(LoginRequest authRequest) {
+        log.info("[AuthService.login]");
+
+        String email = authRequest.getEmail();
+
+        // TODO: 1. 이메일 유효성 확인
+        long userId;
+        try {
+            userId = userDao.getUserIdByEmail(email);
+        } catch (IncorrectResultSizeDataAccessException e) {
+            throw new UserException(EMAIL_NOT_FOUND);
+        }
+
+        // TODO: 2. 비밀번호 일치 확인
+        validatePassword(authRequest.getPassword(), userId);
+
+        // TODO: 3. JWT 갱신
+        String updatedJwt = jwtTokenProvider.createToken(email, userId);
+
+        return new LoginResponse(userId, updatedJwt);
+    }
+
+    private void validatePassword(String password, long userId) {
+        String encodedPassword = userDao.getPasswordByUserId(userId);
+        if (!passwordEncoder.matches(password, encodedPassword)) {
+            throw new UserException(PASSWORD_NO_MATCH);
+        }
+    }
+
+    public long getUserIdByEmail(String email) {
+        try {
+            return userDao.getUserIdByEmail(email);
+        } catch (IncorrectResultSizeDataAccessException e) {
+            throw new JwtUnauthorizedTokenException(TOKEN_MISMATCH);
+        }
+    }
+
+}
