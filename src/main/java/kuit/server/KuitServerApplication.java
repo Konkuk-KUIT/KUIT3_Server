b65574d2b48ae307054e762802f5a90725892112
kmw10693 <kmw10693@konkuk.ac.kr>
2024-05-09 23:17:34 +0900

    feat: initial commit

---
+++ src/main/java/kuit/server/KuitServerApplication.java
@@ -0,0 +1,13 @@
+package kuit.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuitServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kuit/server/KuitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kuit.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class KuitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
