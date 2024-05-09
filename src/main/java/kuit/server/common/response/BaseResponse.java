ea881717b1111b1c4effdd0ab360ba3281bc5dbf
kmw10693 <kmw10693@konkuk.ac.kr>
2024-05-09 23:15:55 +0900

    feat: BaseResponse 구현

---
+++ src/main/java/kuit/server/common/response/BaseResponse.java
@@ -0,0 +1,43 @@
+package kuit.server.common.response;
+
+import kuit.server.common.response.status.ResponseStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+
+import static kuit.server.common.response.status.BaseExceptionResponseStatus.SUCCESS;
+
+@Getter
+@JsonPropertyOrder({"code", "status", "message", "result"})
+public class BaseResponse<T> implements ResponseStatus {
+
+    private final int code;
+    private final int status;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T result;
+
+    public BaseResponse(T result) {
+        this.code = SUCCESS.getCode();
+        this.status = SUCCESS.getStatus();
+        this.message = SUCCESS.getMessage();
+        this.result = result;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public int getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+}
