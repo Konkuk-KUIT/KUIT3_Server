9d2dab0d4433784dabd1f3d902fbdde76d3e1df8
kmw10693 <kmw10693@konkuk.ac.kr>
2024-05-09 23:19:24 +0900

    feat: 스프링 시큐리티 구현

---
+++ src/main/java/kuit/server/config/WebConfig.java
@@ -0,0 +1,32 @@
+package kuit.server.config;
+
+import kuit.server.common.argument_resolver.JwtAuthHandlerArgumentResolver;
+import kuit.server.common.interceptor.JwtAuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtAuthInterceptor jwtAuthenticationInterceptor;
+    private final JwtAuthHandlerArgumentResolver jwtAuthHandlerArgumentResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtAuthenticationInterceptor)
+                .order(1)
+                .addPathPatterns("/auth/test");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(jwtAuthHandlerArgumentResolver);
+    }
+
+}
