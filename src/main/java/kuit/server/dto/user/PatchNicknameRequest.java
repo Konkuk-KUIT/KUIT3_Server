0cd9e56cd64e4d8f9e8cd8f32ff104433f72e214
Park99 <sungung1030@gmail.com>
2024-05-23 14:05:34 +0900

    refactor : patchnickname

--- src/main/java/kuit/server/dto/user/PatchNicknameRequest.java
+++ src/main/java/kuit/server/dto/user/PatchNicknameRequest.java
@@ -1,16 +1,19 @@
 package kuit.server.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class PatchNicknameRequest {
 
-    @NotNull(message = "nickname: {NotNull}")
+    @NotBlank(message = "nickname: {NotNull}")  
+    @Length(max = 25, message = "nickname: 최대 {max}자리까지 가능합니다")
     private String nickname;
 
 }
