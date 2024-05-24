4d92996d1d6e263f00550ba6e4451f256560828f
Sang-Min Kang <rdrniel12345@gmail.com>
2024-05-24 17:23:41 +0900

    feat: add valid logic in restaurant controller

--- src/main/java/kuit/server/dto/user/PostRestaurantRequest.java
+++ src/main/java/kuit/server/dto/user/PostRestaurantRequest.java
@@ -1,5 +1,9 @@
 package kuit.server.dto.user;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,13 +13,30 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PostRestaurantRequest {
 
+    @NotBlank(message = "address: {NotBlank}")
     private String address;
+
+    @NotBlank(message = "category: {NotBlank}")
     private String category;
+
+    @Max(value = 90, message = "latitude: 가질 수 있는 최대 값은 90입니다.")
+    @Min(value = 0, message = "latitude: 가질 수 있는 최소 값은 0입니다.")
     private double latitude;
+
+    @Max(value = 180, message = "longitude: 가질 수 있는 최대 값은 180입니다.")
+    @Min(value = -180, message = "longitude: 가질 수 있는 최소 값은 -180입니다.")
     private double longitude;
+
+    @PositiveOrZero(message = "min_price: 최소 금액은 0 이상의 정수입니다.")
     private double min_price;
+
+    @NotBlank(message = "name: {NotBlank}")
     private String name;
+
+    @PositiveOrZero(message = "star: 별점은 0~5의 값입니다.")
+    @Max(value = 5, message = "star: 별점은 0~5의 값입니다.")
     private double star;
+
     private String picture_url;
 
 }
